5fdf0877f18f58112df10890ecb287c0c65cfc9c
estherdds <90476110+estherdds@users.noreply.github.com>
2021-11-30 17:31:33 -0300

    Add files via upload

---
+++ EstherSerpa.sql
@@ -0,0 +1,22 @@
+create database Esther_Serpa;
+use Esther_Serpa;
+
+create table tbl_Login(
+cpf varchar (11) not null primary key,
+email varchar (40) not null,
+senha int not null
+)engine = innodb;
+
+create table tbl_dados(
+id int not null auto_increment primary key,
+eds varchar (100) not null,
+e06 varchar (40) not null,
+s19 varchar (40) not null
+)engine = innodb;
+
+create table tbl_usuario(
+nome varchar (100) not null,
+cpf varchar (11) not null primary key,
+email varchar (40) not null,
+senha varchar (40) not null
+) engine = innodb;
