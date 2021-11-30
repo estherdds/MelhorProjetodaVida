fcd4fedaad599397e675a5f0557db25b1c3597ef
estherdds <90476110+estherdds@users.noreply.github.com>
2021-11-30 17:50:44 -0300

    Update Frm_Login.cs

--- ProjetodaVida/ProjetodaVida/Ui/Frm_Login.cs
+++ ProjetodaVida/ProjetodaVida/Ui/Frm_Login.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,15 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProjetodaVida.CODE.BLL;
+using ProjetodaVida.CODE.DTO;
 
 namespace ProjetodaVida
 {
-    public partial class Form1 : Form
+    public partial class Frm_Login : Form
     {
-        public Form1()
+        LoginDTO LoginDTO = new LoginDTO();
+        LoginBLL LoginBLL = new LoginBLL();
+        public Frm_Login()
         {
             InitializeComponent();
         }
 
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            LoginDTO.Cpf =   txtCpf.Text;
+            LoginDTO.Email = txtEmail.Text;
+            LoginDTO.Senha = txtSenha.Text;
+
+            if (LoginBLL.RealizarLogin(LoginDTO) == true)
+            {
+                Frm_Login Frm_Login = new Frm_Login();
+                Frm_Login.ShowDialog();
+            }
+
+            else
+            {
+                {
+                    MessageBox.Show("Confira se há algo errado.", "Há algo errado.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+
+        }
     }
 }
